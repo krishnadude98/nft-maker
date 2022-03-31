b5346adc0337de8b9c0daf942193fd1b96556434
krishnadude98 <krishnadude98@gmail.com>
2022-03-31 17:36:52 +0530

    working

--- controller/main.js
+++ controller/main.js
@@ -7,6 +7,7 @@ var ipfsAPI = require('ipfs-api');
 var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'});
 const fs = require('fs');
 const cardano = require('./cardano');
+
 const storage= multer.diskStorage({
     destination: function(req,file,cb){
         cb(null,'./images');
@@ -102,11 +103,11 @@ Router.post('/mint',async(req,res)=>{
     console.log(req.query.address);
     const wallet= cardano.wallet("ADA");
     let utxo= wallet.balance().utxo;
-    
+    let {hash,assetName,description,rType}=req.body;
     if(utxo.length==0){
         return res.json({"message":"Make sure to send Ada if already sended pls wait for a few minutes"});
     }
-    let hash =req.body.hash;
+   
     let ipfsLink="ipfs://"+hash;
     // 2. Define mint script
     console.log("IPFS=>",ipfsLink);
@@ -121,91 +122,147 @@ Router.post('/mint',async(req,res)=>{
    
     const POLICY_ID= cardano.transactionPolicyid(mintScript)
     
-    return res.json(POLICY_ID)
+     
+     
     //4.asset name Hex
-    // const ASSET_NAME_HEX = assetName.split("").map(c => c.charCodeAt(0).toString(16).padStart(2, "0")).join("");
+    const ASSET_NAME_HEX = assetName.split("").map(c => c.charCodeAt(0).toString(16).padStart(2, "0")).join("");
     
-    // // 5. Create ASSET_ID which os sum of ASSET_NAME_HEX and POLICY_ID
-    // const ASSET_ID = POLICY_ID + "." + ASSET_NAME_HEX
+     // 5. Create ASSET_ID which os sum of ASSET_NAME_HEX and POLICY_ID
+    const ASSET_ID = POLICY_ID + "." + ASSET_NAME_HEX
+    
+     // 6. Define metadata
 
-    // // 6. Define metadata
-
-    // const metadata = {
-    //     721: {
-    //         [POLICY_ID]: {
-    //             [assetName]: {
-    //                 name: assetName,
-    //                 image: ipfsLink,
-    //                 description: description,
-    //                 type: rType,
+    const metadata = {
+         721: {
+             [POLICY_ID]: {
+                 [assetName]: {
+                     name: assetName,
+                     image: ipfsLink,
+                     description: description,
+                     type: rType,
                
-    //             }
-    //         }
-    //     }
-    // }
-    // console.log("METADATA=>",metadata);
-    // console.log("BEFORE TRANSACTIONS")
-    // // 7. Define transaction
+                 }
+             }
+         }
+     }
+    
+     // 7. Define transaction
 
-    // const tx = {
-    //     txIn: wallet.balance().utxo,
-    //     txOut: [
-    //         {
-    //             address: wallet.paymentAddr,
-    //             value: { ...wallet.balance().value, [ASSET_ID]: 1 }
-    //         }
-    //     ],
-    //     mint: [
-    //         { action: "mint", quantity: 1, asset: ASSET_ID, script: mintScript },
-    //     ],
-    //     metadata,
-    //     witnessCount: 2
-    // }
+     const tx = {
+         txIn: wallet.balance().utxo,
+         txOut: [
+             {
+                 address: wallet.paymentAddr,
+                 value: { ...wallet.balance().value, [ASSET_ID]: 1 }
+             }
+         ],
+         mint: [
+             { action: "mint", quantity: 1, asset: ASSET_ID, script: mintScript },
+         ],
+         metadata,
+         witnessCount: 2
+     }
      
 
 
-    // if(Object.keys(tx.txOut[0].value).includes("undefined")|| Object.keys(tx.txIn[0].value.includes("undefinded"))){
-    //     delete tx.txOut[0].value.undefined
-    //     delete tx.txIn[0].value.undefined
-    // }
+     if(Object.keys(tx.txOut[0].value).includes("undefined")|| Object.keys(tx.txIn[0].value.includes("undefinded"))){
+         delete tx.txOut[0].value.undefined
+         delete tx.txIn[0].value.undefined
+     }
 
-    // // 8. Build transaction
+     // 8. Build transaction
 
-    // const buildTransaction = (tx) => {
+     const buildTransaction = (tx) => {
 
-    //     const raw = cardano.transactionBuildRaw(tx)
-    //     const fee = cardano.transactionCalculateMinFee({
-    //         ...tx,
-    //         txBody: raw./controller
-    //     })
+         const raw = cardano.transactionBuildRaw(tx)
+         const fee = cardano.transactionCalculateMinFee({
+             ...tx,
+             txBody: raw
+         })
 
-    //     tx.txOut[0].value.lovelace -= fee
+         tx.txOut[0].value.lovelace -= fee
 
-    //     return cardano.transactionBuildRaw({ ...tx, fee })
-    // }
+         return cardano.transactionBuildRaw({ ...tx, fee })
+     }
 
-    // console.log(tx)
-    // const raw = buildTransaction(tx)
+     console.log(tx)
+     const raw = buildTransaction(tx)
 
-    // // 9. Sign transaction
+     // 9. Sign transaction
 
-    // const signTransaction = (wallet, tx) => {
+     const signTransaction = (wallet, tx) => {
 
-    //     return cardano.transactionSign({
-    //         signingKeys: [wallet.payment.skey, wallet.payment.skey ],
-    //         txBody: tx
-    //     })
-    // }
+         return cardano.transactionSign({
+             signingKeys: [wallet.payment.skey, wallet.payment.skey ],
+             txBody: tx
+         })
+     }
 
-    // const signed = signTransaction(wallet, raw)
-    // console.log("SIGNED=>",signed);
-    // // 10. Submit transaction
+     const signed = signTransaction(wallet, raw)
+     console.log("SIGNED=>",signed);
+     // 10. Submit transaction
 
-    // const txHash = cardano.transactionSubmit(signed)
+     const txHash = cardano.transactionSubmit(signed)
 
-    // res.json({"message":"Transaction Submitted","txHash":txHash});
+     res.json({"message":"Transaction Submitted","txHash":txHash});
 });
 
+
+Router.post('/send',async(req,res)=>{
+    const sender= cardano.wallet("ADA");
+    let receiverAddress=req.body.receiverAddress;
+    let asset= req.body.assetId;
+    let obj={};
+   
+    
+    
+    const txInfo = {
+        txIn: cardano.queryUtxo(sender.paymentAddr),
+        txOut: [
+          {
+            address: sender.paymentAddr,
+            value: {
+              lovelace: sender.balance().value.lovelace - cardano.toLovelace(1.6),
+            },
+          },
+          {
+            address: receiverAddress,
+
+            value: {
+              lovelace: cardano.toLovelace(1.6),
+              [asset]:1
+            },
+          },
+        ],
+      };
+    console.log(txInfo.txOut);
+
+    const raw = cardano.transactionBuildRaw(txInfo);
+
+    const fee = cardano.transactionCalculateMinFee({
+        ...txInfo,
+        txBody: raw,
+        witnessCount: 1,
+    });
+
+    //pay the fee by subtracting it from the sender utxo
+    txInfo.txOut[0].value.lovelace -= fee;
+
+    //create final transaction
+    const tx = cardano.transactionBuildRaw({ ...txInfo, fee });
+
+    //sign the transaction
+    const txSigned = cardano.transactionSign({
+        txBody: tx,
+        signingKeys: [sender.payment.skey],
+    });
+
+    //submit transaction
+    const txHash = cardano.transactionSubmit(txSigned);
+    res.json({"message":"Transfer Transaction Submitted","txHash":txHash});
+      
+
+})
 
 
 
